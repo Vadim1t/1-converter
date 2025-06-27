34327acc7df57a1c2310fc0ca2cb212240b05d76
Vadim Khilko <khilvad@gmail.com>
2025-06-27 10:23:08 +0700

    Сделал ДЗ 4.11

--- main.go
+++ main.go
@@ -3,8 +3,20 @@ package main
 import "fmt"
 
 func main() {
+	user()
 	const usdToEuro = 0.86
 	const usdToRub = 78.25
 	const euroToRub = (1 / usdToEuro) * usdToRub
 	fmt.Print(euroToRub)
+}
+
+func user() string {
+	var name string
+	fmt.Printf("Здравствуйте! Представьтесь пожалуйста: ")
+	fmt.Scan(&name)
+	return name
+}
+
+func convert(amount float64, from string, to string) float64 {
+	return 0
 }
